f093cf107c72ec5a571831a54f1c993646019896
Safandhi Yonathan <s.jonathan86@gmail.com>
2014-07-29 23:46:32 +0700

    edit index.html tambah file landing-page-1.html, function.js

--- js/function.js
+++ js/function.js
@@ -1,14 +1,23 @@
 $(document).ready(function(){
     //CLICK CATEGORY
-   $('.list-category .btn-block').click(function() {
+   $('.list-category .btn-block').click(function(event) {
+       event.stopPropagation();
       var temp = $(this);
       var filterNum = temp.parents("list-category").attr('class').match(/\d+/);
-      $('.catg-' + filterNum + " .cat-rest").fadeIn();
+      $('.cat-rest-' + filterNum).fadeIn();
 
 //      $('.panel.close_more').click(function() {
 //         $('.ph_desc.phd_' + filterNum).fadeOut();
 //      });
 alert(filterNum);
       return false;
+   });
+   $(document).click(function(event){
+      var inpsearch = $('ul.wrp_search');
+      var clks = $('.block_htop ul li .click_search');
+      
+      if(inpsearch.has(event.target).length === 0 && clks.has(event.target).length === 0) {
+         inpsearch.animate({'width':'0'}).hide("slow");
+      }
    });
 });//END DOCUMENT
