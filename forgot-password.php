c0060474858cc80f46e4edd0a7ddd16ca225a6a5
Safandhi Yonathan <s.jonathan86@gmail.com>
2014-08-08 21:51:31 +0700

    -edit forgot password, reset password -replace kuskas.pro to anyX

---
+++ forgot-password.php
@@ -0,0 +1,28 @@
+<?php
+include 'inc_header.php';
+include 'main-menu-before-login.php';
+?>
+<div class="container">
+    <div class="row">
+        <div class="panel">
+            <form class="form-center form-horizontal" method="post" action="" name="" role="form">
+                <h3 class="append_bottom_3 text-center">Change your password</h3>
+                <div class="form-group">
+                    <input class="form-control" type="password" name="" value="" placeholder="Current password"/>
+                </div>
+                <div class="form-group">
+                    <input class="form-control" type="password" name="" value="" placeholder="New password"/>
+                </div>
+                <div class="form-group append_bottom_3">
+                    <input class="form-control" type="password" name="" value="" placeholder="New password"/>
+                </div>
+                <div class="form-horizontal upfix_2">
+                    <div class="form-group text-center">
+                        <button type="submit" class="btn btn-success">Save</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div><!--end row-->
+</div>
+<?php include 'inc_footer.php'; ?>
--- reset-password.php
+++ reset-password.php
@@ -8,15 +8,15 @@ include 'main-menu-before-login.php';
             <form class="form-center form-horizontal" method="post" action="" name="" role="form">
                 <h3 class="append_bottom_3 text-center">Reset your password</h3>
                 <div class="form-group has-error">
-                    <input type="password" class="form-control" id="" name="" placeholder="Enter your old password">
+                    <input type="password" class="form-control" id="" name="" placeholder="Current password">
                     <span class="control-label">This field is required.</span>
                 </div>
                 <div class="form-group has-error">
-                    <input type="password" class="form-control" id="" name="" placeholder="Enter your new password">
+                    <input type="password" class="form-control" id="" name="" placeholder="New password">
                     <span class="control-label">This field is required.</span>
                 </div>
                 <div class="form-group has-error">
-                    <input type="password" class="form-control" id="" name="" placeholder="Confirm your new password">
+                    <input type="password" class="form-control" id="" name="" placeholder="New password">
                     <span class="control-label">This field is required.</span>
                 </div>
                 <div class="form-horizontal upfix_2">
