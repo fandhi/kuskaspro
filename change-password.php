6145be2fd84d5743378b880439a87ae249968979
Safandhi Yonathan <s.jonathan86@gmail.com>
2014-08-08 22:52:02 +0700

    -add new file, change-password.php

---
+++ change-password.php
@@ -0,0 +1,31 @@
+<?php
+include 'inc_header.php';
+include 'main-menu-after-login.php';
+?>
+<div class="container">
+    <div class="row">
+        <div class="panel">
+            <form class="form-center form-horizontal" method="post" action="" name="" role="form">
+                <h3 class="append_bottom_3 text-center">Change your password</h3>
+                <div class="form-group">
+                    <input type="password" class="form-control" id="" name="" placeholder="Input your old password">
+                    <!--<span class="control-label">This field is required.</span>-->
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" id="" name="" placeholder="Input new password">
+                    <!--<span class="control-label">This field is required.</span>-->
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" id="" name="" placeholder="Confirm new password">
+                    <!--<span class="control-label">This field is required.</span>-->
+                </div>
+                <div class="form-horizontal upfix_2">
+                    <div class="form-group text-center">
+                        <button type="submit" class="btn btn-success">Save</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div><!--end row-->
+</div>
+<?php include 'inc_footer.php'; ?>
