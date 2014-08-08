6e062f58f9bdbdfa1476982f7de7694385224105
Safandhi Yonathan <s.jonathan86@gmail.com>
2014-08-09 01:27:22 +0700

    -add file: search-result.php

---
+++ search-result.php
@@ -0,0 +1,101 @@
+<?php
+include 'inc_header.php';
+include 'main-menu-after-login.php';
+?>
+<div class="container">
+    <div class="row">
+        <div class=""></div>
+        <div class="col-xs-12 col-sm-4 col-md-3 well">
+            <form class="form-horizontal col-xs-12" method="post" action="" role="form">
+                <label>Narrow result by</label>
+                <div class="form-group">
+                    <select class="form-control">
+                        <option value="">Province</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <select class="form-control">
+                        <option value="">City</option>
+                    </select>
+                </div>
+                <label>Sort by salary</label>
+                <div class="form-group">
+                    <select class="form-control">
+                        <option value="">Highest</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <input class="btn btn-primary" type="submit" value="Submit">
+                </div>
+            </form>
+        </div>
+        <div class="col-sm-8 col-md-9">
+            <ul class="media-list">
+                <li class="media">
+                    <a class="pull-left" href="#">
+                        <img class="media-object" data-src="holder.js/64x64" alt="64x64" src="img/content/profile/creative-person.jpg" style="width: 100px; height: 100px;">
+                    </a>
+                    <div class="media-body">
+                        <h4 class="media-heading"><a href="">Suparjo</a></h4>
+                        <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+                        <div class="">
+                            <div class="col-lg-5 prefix_0">
+                                <b>IDR 500.000 per Jam Bandung, Jawa Barat</b>
+                            </div>
+                            <div class="col-lg-5">
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="media">
+                    <a class="pull-left" href="#">
+                        <img class="media-object" data-src="holder.js/64x64" alt="64x64" src="img/content/profile/creative-person.jpg" style="width: 100px; height: 100px;">
+                    </a>
+                    <div class="media-body">
+                        <h4 class="media-heading"><a href="">Suparjo</a></h4>
+                        <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+                        <div class="">
+                            <div class="col-lg-5 prefix_0">
+                                <b>IDR 500.000 per Jam Bandung, Jawa Barat</b>
+                            </div>
+                            <div class="col-lg-5">
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="media">
+                    <a class="pull-left" href="#">
+                        <img class="media-object" data-src="holder.js/64x64" alt="64x64" src="img/content/profile/creative-person.jpg" style="width: 100px; height: 100px;">
+                    </a>
+                    <div class="media-body">
+                        <h4 class="media-heading"><a href="">Suparjo</a></h4>
+                        <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+                        <div class="">
+                            <div class="col-lg-5 prefix_0">
+                                <b>IDR 500.000 per Jam Bandung, Jawa Barat</b>
+                            </div>
+                            <div class="col-lg-5">
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div><!--end row-->
+</div>
+<?php include 'inc_footer.php'; ?>
