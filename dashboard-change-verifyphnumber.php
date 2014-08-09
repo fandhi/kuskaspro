45445c525b65b0fe3f18fca4c990b0545d047874
Safandhi Yonathan <s.jonathan86@gmail.com>
2014-08-09 15:56:25 +0700

    -edit active sidemenu

--- dashboard-change-verifyphnumber.php
+++ dashboard-change-verifyphnumber.php
@@ -6,9 +6,9 @@ include 'main-menu-after-login.php';
     <div class="row">
         <div class="col-sm-4 col-md-3">
             <div class="list-group">
-                <a href="dashboard-manage-profile.php" class="list-group-item active">Manage Profile</a>
+                <a href="dashboard-manage-profile.php" class="list-group-item">Manage Profile</a>
                 <a href="dashboard-change-password.php" class="list-group-item">Change Password</a>
-                <a href="dashboard-change-verifyphnumber.php" class="list-group-item">Change/verify ph.number</a>
+                <a href="dashboard-change-verifyphnumber.php" class="list-group-item active">Change/verify ph.number</a>
                 <a href="dashboard-go-premium.php" class="list-group-item">Go Premium</a>
             </div>
         </div>
