5a23abd15edd8063866a2f578d1ce5c59f5af708
Safandhi Yonathan <s.jonathan86@gmail.com>
2014-08-15 00:05:44 +0700

    -tambah file list-category.php -edit css

---
+++ list-category.php
@@ -0,0 +1,112 @@
+<?php
+include 'inc_header.php';
+include 'main-menu-after-login.php';
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-sm-4 col-md-3">
+            <div class="panel panel-primary inverse-theme menu-list-category">
+                <div class="panel-heading"><h3 class="panel-title">House Keeping</h3></div>
+                <div class="panel-body">
+                    <ul class="list-group inverse-theme">
+                        <li><a class="list-group-item" href="" title="">Tukang kebun <span class="badge">(10)</span></a></li>
+                        <li><a class="list-group-item" href="" title="">Tukang kuras koran <span class="badge">(10)</span></a></li>
+                        <li><a class="list-group-item" href="" title="">Pembantu rumah tangga <span class="badge">(10)</span></a></li>
+                        <li><a class="list-group-item" href="" title="">Tukang sedot wc <span class="badge">(10)</span></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-8 col-md-9">
+            <ul id="search-result" class=" media-list">
+                <li class="media result-1">
+                    <a class="pull-left" href="#">
+                        <img class="media-object" data-src="holder.js/64x64" alt="64x64" src="img/content/profile/creative-person.jpg" style="width: 100px; height: 100px;">
+                    </a>
+                    <div class="media-body">
+                        <h4 class="media-heading"><a href="">Suparjo - Tukang kuras toren</a></h4>
+                        <div class="result-toggle">
+                            <article>
+                                <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+                                <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+                                <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+                            </article>
+                        </div>
+                        <button class="btn btn-info btn-xs append_top_1 btn_read_more" type="button">More...</button>
+                        <button class="btn btn-info btn-xs append_top_1 btn_less" type="button">Less</button>
+                        <div class="upfix_1 ket-star">
+                            <div class="col-md-2 prefix_0 pasfix_0 pupline">
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                            </div>
+                            <div class="col-lg-6 prefix_0">
+                                <b><span class="pupline">IDR 500.000 per Jam</span> Bandung, Jawa Barat</b>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="media result-2">
+                    <a class="pull-left" href="#">
+                        <img class="media-object" data-src="holder.js/64x64" alt="64x64" src="img/content/profile/creative-person.jpg" style="width: 100px; height: 100px;">
+                    </a>
+                    <div class="media-body">
+                        <h4 class="media-heading"><a href="">Suparjo - Tukang kebun</a></h4>
+                        <div class="result-toggle">
+                            <article>
+                                <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+                                <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+                                <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+                            </article>
+                        </div>
+                        <button class="btn btn-info btn-xs append_top_1 btn_read_more" type="button">More...</button>
+                        <button class="btn btn-info btn-xs append_top_1 btn_less" type="button">Less</button>
+                        <div class="upfix_1 ket-star">
+                            <div class="col-md-2 prefix_0 pasfix_0 pupline">
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                            </div>
+                            <div class="col-lg-6 prefix_0">
+                                <b><span class="pupline">IDR 500.000 per Jam</span> Bandung, Jawa Barat</b>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="media result-3">
+                    <a class="pull-left" href="#">
+                        <img class="media-object" data-src="holder.js/64x64" alt="64x64" src="img/content/profile/creative-person.jpg" style="width: 100px; height: 100px;">
+                    </a>
+                    <div class="media-body">
+                        <h4 class="media-heading"><a href="">Suparman - Pembantu rumah tangga</a></h4>
+                        <div class="result-toggle">
+                            <article>
+                                <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+                                <p>tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+                            </article>
+                        </div>
+                        <button class="btn btn-info btn-xs append_top_1 btn_read_more" type="button">More...</button>
+                        <button class="btn btn-info btn-xs append_top_1 btn_less" type="button">Less</button>
+                        <div class="upfix_1 ket-star">
+                            <div class="col-md-2 prefix_0 pasfix_0 pupline">
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                            </div>
+                            <div class="col-lg-6 prefix_0">
+                                <b><span class="pupline">IDR 500.000 per Jam</span> Bandung, Jawa Barat</b>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div><!--end row-->
+</div>
+<?php include 'inc_footer.php'; ?>
