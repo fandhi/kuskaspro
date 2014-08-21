623b3196cbc4cafb4664829b8d3d32fd844b06bc
Safandhi Yonathan <s.jonathan86@gmail.com>
2014-08-22 01:37:47 +0700

    -tambah file untuk view categories

---
+++ other-categories.php
@@ -0,0 +1,90 @@
+<?php
+include 'inc_header.php';
+include 'main-menu-after-login.php';
+?>
+<div class="container">
+    <div id="catBox" class="row">
+        <div class="col-sm-6 col-md-3">
+            <div class="thumbnail view view-first">
+                <img src="img/material/tukang-cat.jpg" alt="...">
+                <div class="caption mask">
+                    <h3>Tukang Cat</h3>
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium</p>
+                    <a href="#" class="btn btn-default" role="button">Button</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-3">
+            <div class="thumbnail view view-first">
+                <img src="img/material/akuntan.jpg" alt="...">
+                <div class="caption mask">
+                    <h3>Akuntan</h3>
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium</p>
+                    <a href="#" class="btn btn-default" role="button">Button</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-3">
+            <div class="thumbnail view view-first">
+                <img src="img/material/tukang-kayu.jpg" alt="...">
+                <div class="caption mask">
+                    <h3>Tukang Kayu</h3>
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium</p>
+                    <a href="#" class="btn btn-default" role="button">Button</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-3">
+            <div class="thumbnail view view-first">
+                <img src="img/material/dokter-umum.jpg" alt="...">
+                <div class="caption mask">
+                    <h3>Dokter Umum</h3>
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium</p>
+                    <a href="#" class="btn btn-default" role="button">Button</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-3">
+            <div class="thumbnail view view-first">
+                <img src="img/material/tukang-cat.jpg" alt="...">
+                <div class="caption mask">
+                    <h3>Tukang Cat</h3>
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium</p>
+                    <a href="#" class="btn btn-default" role="button">Button</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-3">
+            <div class="thumbnail view view-first">
+                <img src="img/material/akuntan.jpg" alt="...">
+                <div class="caption mask">
+                    <h3>Akuntan</h3>
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium</p>
+                    <a href="#" class="btn btn-default" role="button">Button</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-3">
+            <div class="thumbnail view view-first">
+                <img src="img/material/tukang-kayu.jpg" alt="...">
+                <div class="caption mask">
+                    <h3>Tukang Kayu</h3>
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium</p>
+                    <a href="#" class="btn btn-default" role="button">Button</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-3">
+            <div class="thumbnail view view-first">
+                <img src="img/material/dokter-umum.jpg" alt="...">
+                <div class="caption mask">
+                    <h3>Dokter Umum</h3>
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium</p>
+                    <a href="#" class="btn btn-default" role="button">Button</a>
+                </div>
+            </div>
+        </div>
+    </div><!--/catBox-->
+    <!--end row-->
+</div>
+<?php include 'inc_footer.php'; ?>
---
+++ view-all-categories.php
@@ -0,0 +1,144 @@
+<?php
+include 'inc_header.php';
+include 'main-menu-before-login.php';
+?>
+<div class="container">
+    <div id="catList" class="row">
+        <div class="list-cat col-sm-6 col-md-4">
+            <section>
+                <h4 class="">Title</h4>
+                <ul>
+                    <li><a href="">All Web Development</a></li>
+                    <li><a href="">Web Design</a></li>
+                    <li><a href="">Web Programming</a></li>
+                    <li><a href="">Ecommerce</a></li>
+                    <li><a href="">UI Design</a></li>
+                    <li><a href="">Website QA</a></li>
+                    <li><a href="">Website Project Management</a></li>
+                    <li><a href="">Other - Web Development</a></li>
+                    <li><a href="">Website QA</a></li>
+                    <li><a href="">Website Project Management</a></li>
+                    <li><a href="">Web Design</a></li>
+                    <li><a href="">Web Programming</a></li>
+                    <li><a href="other-categories.php">Other - </a></li>
+                </ul>
+            </section>
+            <section>
+                <h4 class="">Title</h4>
+                <ul>
+                    <li><a href="">All Web Development</a></li>
+                    <li><a href="">Web Design</a></li>
+                    <li><a href="">Web Programming</a></li>
+                    <li><a href="">Ecommerce</a></li>
+                    <li><a href="">UI Design</a></li>
+                    <li><a href="">Website QA</a></li>
+                    <li><a href="">Website Project Management</a></li>
+                    <li><a href="other-categories.php">Other - </a></li>
+                </ul>
+            </section>
+            <section>
+                <h4 class="">Title</h4>
+                <ul>
+                    <li><a href="">All Web Development</a></li>
+                    <li><a href="">Web Design</a></li>
+                    <li><a href="">Web Programming</a></li>
+                    <li><a href="">Ecommerce</a></li>
+                    <li><a href="">UI Design</a></li>
+                    <li><a href="">Website QA</a></li>
+                    <li><a href="">Website Project Management</a></li>
+                    <li><a href="other-categories.php">Other - </a></li>
+                </ul>
+            </section>
+
+        </div>
+        <div class="list-cat col-sm-6 col-md-4">
+            <section>
+                <h4 class="">Title</h4>
+                <ul>
+                    <li><a href="">All Web Development</a></li>
+                    <li><a href="">Web Design</a></li>
+                    <li><a href="">Web Programming</a></li>
+                    <li><a href="">Ecommerce</a></li>
+                    <li><a href="">UI Design</a></li>
+                    <li><a href="">Website QA</a></li>
+                    <li><a href="">Website Project Management</a></li>
+                    <li><a href="other-categories.php">Other - </a></li>
+                </ul>
+            </section>
+            <section>
+                <h4 class="">Title</h4>
+                <ul>
+                    <li><a href="">All Web Development</a></li>
+                    <li><a href="">Web Design</a></li>
+                    <li><a href="">Web Programming</a></li>
+                    <li><a href="">Ecommerce</a></li>
+                    <li><a href="">UI Design</a></li>
+                    <li><a href="">Website QA</a></li>
+                    <li><a href="">Website Project Management</a></li>
+                    <li><a href="other-categories.php">Other - </a></li>
+                </ul>
+            </section>
+            <section>
+                <h4 class="">Title</h4>
+                <ul>
+                    <li><a href="">All Web Development</a></li>
+                    <li><a href="">Web Design</a></li>
+                    <li><a href="">Web Programming</a></li>
+                    <li><a href="">Ecommerce</a></li>
+                    <li><a href="">UI Design</a></li>
+                    <li><a href="">Website QA</a></li>
+                    <li><a href="">Website Project Management</a></li>
+                    <li><a href="other-categories.php">Other - </a></li>
+                </ul>
+            </section>
+
+        </div>
+        <div class="list-cat col-sm-6 col-md-4">
+            <section>
+                <h4 class="">Title</h4>
+                <ul>
+                    <li><a href="">All Web Development</a></li>
+                    <li><a href="">Web Design</a></li>
+                    <li><a href="">Web Programming</a></li>
+                    <li><a href="">Ecommerce</a></li>
+                    <li><a href="">UI Design</a></li>
+                    <li><a href="">Website QA</a></li>
+                    <li><a href="">Website Project Management</a></li>
+                    <li><a href="other-categories.php">Other - </a></li>
+                </ul>
+            </section>
+            <section>
+                <h4 class="">Title</h4>
+                <ul>
+                    <li><a href="">All Web Development</a></li>
+                    <li><a href="">Web Design</a></li>
+                    <li><a href="">Web Programming</a></li>
+                    <li><a href="">Ecommerce</a></li>
+                    <li><a href="">UI Design</a></li>
+                    <li><a href="">Website QA</a></li>
+                    <li><a href="">Website Project Management</a></li>
+                    <li><a href="">Other - Web Development</a></li>
+                    <li><a href="">Website QA</a></li>
+                    <li><a href="">Website Project Management</a></li>
+                    <li><a href="other-categories.php">Other - </a></li>
+                </ul>
+            </section>
+            <section>
+                <h4 class="">Title</h4>
+                <ul>
+                    <li><a href="">All Web Development</a></li>
+                    <li><a href="">Web Design</a></li>
+                    <li><a href="">Web Programming</a></li>
+                    <li><a href="">Ecommerce</a></li>
+                    <li><a href="">UI Design</a></li>
+                    <li><a href="">Website QA</a></li>
+                    <li><a href="">Website Project Management</a></li>
+                    <li><a href="other-categories.php">Other - </a></li>
+                </ul>
+            </section>
+
+        </div>
+    </div>
+    <!--end row-->
+</div>
+<?php include 'inc_footer.php'; ?>
